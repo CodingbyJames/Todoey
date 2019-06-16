30db338123686c3ae040c256c3ccaf831d554762
James Garcia <codingbyjames@gmail.com>
2019-06-15 22:01:22 -0700

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by James Garcia-Bengochea on 6/15/19.
+//  Copyright © 2019 James Garcia-Bengochea. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
